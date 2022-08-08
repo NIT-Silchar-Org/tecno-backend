7ac8a53f525c2419519d2cdba3aa46d4f585c349
Pratik Majumdar (codadept) <info.pratikmajumdar@gmail.com>
2022-08-08 20:15:17 +0530

    ADD: Merge models

--- src/controllers/home/say.ts
+++ src/controllers/home/say.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 
-// import * as Errors from "@errors"
-import * as Success from "@success";
+import * as Errors from "@errors";
+// import * as Success from "@success";
 
 function sayHello(_: Request, res: Response) {
-  res.json(Success.Home.hello);
-  // res.json(.System.serverError);
+  // res.json(Success.Home.hello);
+  res.json(Errors.System.serverError);
 }
 
 export { sayHello };
