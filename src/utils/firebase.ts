31f3cbc312e6928bb1f0fbc9b1c9b41842c4267a
Saurav Pal <resyfer.dev@gmail.com>
2022-08-15 20:59:43 +0530

    UPD: Firebase creds to env

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,7 +2,11 @@ import * as admin from "firebase-admin";
 // import serviceAccount from "../../firebase_secret.json";
 
 const firebaseAdmin: admin.app.App = admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SECRET!)),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+  }),
 });
 
 export default firebaseAdmin;
