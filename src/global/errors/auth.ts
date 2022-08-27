6c6c7388773b7b0edc4c1ce03af4b73ea1d7e466
Pratik Majumdar <63517465+codadept@users.noreply.github.com>
2022-08-27 17:41:41 +0530

    Draft: Transaction (#7)
    * FIX: Transaction cooldown, auth
    
    * FIX: Transaction comments, renaming

--- src/global/errors/auth.ts
+++ src/global/errors/auth.ts
@@ -3,5 +3,6 @@ import * as Utils from "@utils";
 const firebaseAuthError = Utils.Response.Error(
   "Firebase authentication error."
 );
+const adminAuthError = Utils.Response.Error("Admin unauthorized.");
 
-export { firebaseAuthError };
+export { firebaseAuthError, adminAuthError };
