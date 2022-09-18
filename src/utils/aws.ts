a4f921417d8434f488cd2755b9548d55ca1f72d3
Saurav Pal <resyfer.dev@gmail.com>
2022-09-18 19:36:03 +0530

    FIX: AWS S3 SDK

--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -1,8 +1,8 @@
-import AWS from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
-let s3: AWS.S3Client;
+let s3: S3Client;
 function initializeAWS() {
-  s3 = new AWS.S3({
+  s3 = new S3Client({
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
