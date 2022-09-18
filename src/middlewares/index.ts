2bd2e75f9f7ad6b1887a3b31f34a5b675a170c9e
Saurav Pal <resyfer.dev@gmail.com>
2022-09-18 19:07:56 +0530

    ADD: Upload error middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,5 +3,6 @@ import * as Module from "./module";
 import * as Auth from "./auth";
 import * as Event from "./event";
 import * as Team from "./team";
+import * as Upload from "./upload";
 
-export { Transaction, Module, Auth, Event, Team };
+export { Transaction, Module, Auth, Event, Team, Upload };
