e176a46a11b8543bca01c5887c5430497d4dc0ad
Saurav Pal <resyfer.dev@gmail.com>
2022-10-05 17:10:46 +0530

    UPD: Checks of team registration atomic as well

--- src/controllers/team/register.ts
+++ src/controllers/team/register.ts
@@ -27,10 +27,6 @@ const registerTeam: Interfaces.Controller.Async = async (req, res, next) => {
   // Any duplicate members, including leader,
   // if duplicate is present, gets removed.
 
-  if (members.size !== memberArray.length) {
-    return next(Errors.Team.memberDuplicates);
-  }
-
   // Get event
   const event = await prisma.event.findFirst({
     where: {
@@ -57,113 +53,104 @@ const registerTeam: Interfaces.Controller.Async = async (req, res, next) => {
     },
   });
 
-  // Check time
-  const now = new Date();
-  if (now < event!.registrationStartTime || now > event!.registrationEndTime) {
-    return next(Errors.Team.timeNotRight);
-  }
+  await prisma.$transaction(async (prisma) => {
+    if (members.size !== memberArray.length) {
+      return next(Errors.Team.memberDuplicates);
+    }
 
-  if (!event) {
-    return next(Errors.Event.eventDoesntExist);
-  }
+    // Check time
+    const now = new Date();
+    if (
+      now < event!.registrationStartTime ||
+      now > event!.registrationEndTime
+    ) {
+      return next(Errors.Team.timeNotRight);
+    }
 
-  // Check member limit
-  if (event!.minTeamSize > members.size || event!.maxTeamSize < members.size) {
-    return next(Errors.Team.teamSizeNotAllowed);
-  }
+    if (!event) {
+      return next(Errors.Event.eventDoesntExist);
+    }
 
-  // Check team name
-  const teamTaken = await prisma.team.count({
-    where: {
-      eventId,
-      teamName: name,
-      OR: [
-        {
-          registrationStatus: RegistrationStatus.REGISTERED,
-        },
-        {
-          registrationStatus: RegistrationStatus.PENDING,
-        },
-      ],
-    },
-    take: 1,
-  });
+    // Check member limit
+    if (
+      event!.minTeamSize > members.size ||
+      event!.maxTeamSize < members.size
+    ) {
+      return next(Errors.Team.teamSizeNotAllowed);
+    }
 
-  if (teamTaken !== 0) {
-    return next(Errors.Team.teamAlreadyExists);
-  }
-
-  // Check if users exist and if registered in another team.
-  for await (const member of members) {
-    const user = await prisma.user.count({
-      where: { username: member },
+    // Check team name
+    const teamTaken = await prisma.team.count({
+      where: {
+        eventId,
+        teamName: name,
+        OR: [
+          {
+            registrationStatus: RegistrationStatus.REGISTERED,
+          },
+          {
+            registrationStatus: RegistrationStatus.PENDING,
+          },
+        ],
+      },
       take: 1,
     });
 
-    if (user === 0) {
-      return next(Errors.User.userNotFound);
+    if (teamTaken !== 0) {
+      return next(Errors.Team.teamAlreadyExists);
     }
 
-    const belongsToTeam = await prisma.teamRegistration.count({
-      where: {
-        registrationStatus: RegistrationStatus.REGISTERED,
-        user: {
-          username: member,
-        },
-        team: {
-          event: {
-            id: eventId,
+    // Check if users exist and if registered in another team.
+    for await (const member of members) {
+      const user = await prisma.user.count({
+        where: { username: member },
+        take: 1,
+      });
+
+      if (user === 0) {
+        return next(Errors.User.userNotFound);
+      }
+
+      const belongsToTeam = await prisma.teamRegistration.count({
+        where: {
+          registrationStatus: RegistrationStatus.REGISTERED,
+          user: {
+            username: member,
+          },
+          team: {
+            event: {
+              id: eventId,
+            },
           },
         },
-      },
-      take: 1,
-    });
+        take: 1,
+      });
 
-    if (belongsToTeam !== 0) {
-      return next(Errors.Team.userAlreadyRegistered);
+      if (belongsToTeam !== 0) {
+        return next(Errors.Team.userAlreadyRegistered);
+      }
     }
-  }
 
-  // Creating a connection array after modifying data from front-end
-  const memberRegistration: Prisma.TeamRegistrationCreateWithoutTeamInput[] =
-    [];
+    // Creating a connection array after modifying data from front-end
+    const memberRegistration: Prisma.TeamRegistrationCreateWithoutTeamInput[] =
+      [];
 
-  members.forEach((member) => {
-    memberRegistration.push({
-      registrationStatus:
-        member === req.user!.username
-          ? RegistrationStatus.REGISTERED
-          : RegistrationStatus.PENDING,
-      role:
-        member === req.user!.username
-          ? TeamMemberRole.LEADER
-          : TeamMemberRole.MEMBER,
-      user: {
-        connect: {
-          username: member,
-        },
-      },
-    });
-  });
-
-  await prisma.$transaction(async (prisma) => {
-    await prisma.team.create({
-      data: {
-        teamName: name,
+    members.forEach((member) => {
+      memberRegistration.push({
         registrationStatus:
-          members.size === 1
+          member === req.user!.username
             ? RegistrationStatus.REGISTERED
             : RegistrationStatus.PENDING,
-        event: {
+        role:
+          member === req.user!.username
+            ? TeamMemberRole.LEADER
+            : TeamMemberRole.MEMBER,
+        user: {
           connect: {
-            id: eventId,
+            username: member,
           },
         },
-        members: {
-          create: memberRegistration,
-        },
-        extraInformation: extraInformation,
-      },
+      });
     });
 
     // Registration incentive if team size is 1.
@@ -218,44 +205,63 @@ const registerTeam: Interfaces.Controller.Async = async (req, res, next) => {
         },
       });
     }
-  });
 
-  // Send Emails to User
-  for await (const member of members) {
-    const user = await prisma.user.findFirst({
-      where: {
-        username: member,
+    await prisma.team.create({
+      data: {
+        teamName: name,
+        registrationStatus:
+          members.size === 1
+            ? RegistrationStatus.REGISTERED
+            : RegistrationStatus.PENDING,
+        event: {
+          connect: {
+            id: eventId,
+          },
+        },
+        members: {
+          create: memberRegistration,
+        },
+        extraInformation: extraInformation,
       },
     });
 
-    if (!user) {
-      return next(Errors.User.userNotFound);
-    }
-
-    let html;
-    let subject;
-    if (member === req.user!.username) {
-      html = Utils.HTML.createRegisterCreationHTML({
-        eventName: event.name,
-        leaderName: user.firstName,
-        moduleName: module!.name,
-        teamName: name,
+    // Send Emails to User
+    for await (const member of members) {
+      const user = await prisma.user.findFirst({
+        where: {
+          username: member,
+        },
       });
 
-      subject = `Team Registration Application Submitted | ${process.env.NAME}`;
-    } else {
-      html = Utils.HTML.createRegisterInvitationHTML({
-        eventName: event.name,
-        leaderName: user.firstName,
-        moduleName: module!.name,
-      });
-      subject = `Team Invitation for ${name} | ${process.env.NAME}`;
+      if (!user) {
+        return next(Errors.User.userNotFound);
+      }
+
+      let html;
+      let subject;
+      if (member === req.user!.username) {
+        html = Utils.HTML.createRegisterCreationHTML({
+          eventName: event!.name,
+          leaderName: user.firstName,
+          moduleName: module!.name,
+          teamName: name,
+        });
+
+        subject = `Team Registration Application Submitted | ${process.env.NAME}`;
+      } else {
+        html = Utils.HTML.createRegisterInvitationHTML({
+          eventName: event!.name,
+          leaderName: user.firstName,
+          moduleName: module!.name,
+        });
+        subject = `Team Invitation for ${name} | ${process.env.NAME}`;
+      }
+
+      Utils.Email.sendMail(user.email, html, subject); // Await Not Used On Purpose
     }
 
-    Utils.Email.sendMail(user.email, html, subject); // Await Not Used On Purpose
-  }
-
-  return res.json(Success.Team.teamCreated);
+    return res.json(Success.Team.teamCreated);
+  });
 };
 
 export { registerTeam };
